7259d4221efb64de8992562dd7508770a20c53f3
caleb wheeler <caleb.edward.wheeler@gmail.com>
2026-06-25 14:55:35 -0500

    implement: funcs that handle completing and failing workflows, move error logging to Worker.Start, but build errors in Fail funcs. refactor: job retries and timeout to be dependency injected into Worker. create: reusable constants. refactor: JobStore.Fail func and handle some job.go cleanup.

--- storage/postgres/workflow.go
+++ storage/postgres/workflow.go
@@ -2,10 +2,18 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	StatusComplete string = "completed"
+	StatusFailed   string = "failed"
+	StatusPending  string = "pending"
+	StatusRunning  string = "running"
 )
 
 type WorkflowStore struct {
@@ -32,20 +40,41 @@ func (ws *WorkflowStore) CreateWorkflow(ctx context.Context, workflowID uuid.UUI
 		current_step,
 		created_at,
 		updated_at
-	) VALUES ($1, $2, $3, $4, $5)`, workflowID, "running", "create_user", time.Now(), time.Now()); err != nil {
+	) VALUES ($1, $2, $3, $4, $5)`, workflowID, StatusRunning, "create_user", time.Now(), time.Now()); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ws *WorkflowStore) Get() bool {
-	return true
+func (ws *WorkflowStore) CompleteWorkflow(ctx context.Context, workflowID uuid.UUID) error {
+	_, err := ws.pool.Exec(ctx, `
+		UPDATE workflows
+		SET
+			status = $1,
+			updated_at = NOW()
+		WHERE id = $2
+	`, StatusComplete, workflowID)
+
+	if err != nil {
+		return fmt.Errorf(
+			"failed to mark workflow %s complete: %w",
+			workflowID,
+			err,
+		)
+	}
+	return nil
 }
 
-func (ws *WorkflowStore) UpdateCurrentStep() bool {
-	return true
-}
-
-func (ws *WorkflowStore) UpdateStatus() bool {
-	return true
+func (ws *WorkflowStore) FailWorkflow(ctx context.Context, workflowID uuid.UUID) error {
+	_, err := ws.pool.Exec(ctx, `
+		UPDATE workflows
+		SET 
+			status = $1,
+			updated_at = NOW()
+		WHERE id = $2
+	`, StatusFailed, workflowID)
+	if err != nil {
+		return fmt.Errorf("failed to mark workflow %s failed: %w", workflowID, err)
+	}
+	return nil
 }
--- workers/workers.go
+++ workers/workers.go
@@ -9,14 +9,18 @@ import (
 	"github.com/CalebEWheeler/StateFlow/storage/postgres"
 )
 
-const JobTimeout = 30
-
 type Worker struct {
-	store *postgres.Store
+	store      *postgres.Store
+	jobTimeout int
+	maxRetries int
 }
 
 func NewWorker(store *postgres.Store) *Worker {
-	return &Worker{store: store}
+	return &Worker{
+		store:      store,
+		jobTimeout: 30,
+		maxRetries: 3,
+	}
 }
 
 func (w *Worker) Start(ctx context.Context) {
@@ -35,39 +39,103 @@ func (w *Worker) Start(ctx context.Context) {
 
 		err = w.process(ctx, job)
 		if err != nil {
-			if failErr := w.store.Job.Fail(
-				ctx,
-				job,
-				err,
-			); failErr != nil {
-				log.Printf(
-					"failed to mark job failed: %v",
-					failErr,
-				)
+			if err := w.HandleExecutionError(ctx, job, err); err != nil {
+				log.Print(err)
 			}
+
 			continue
 		}
 
-		err = w.store.Job.Complete(ctx, job.ID)
-		if err != nil {
-			if failErr := w.store.Job.Fail(
-				ctx,
-				job,
-				fmt.Errorf("failed to complete job: %w", err),
-			); failErr != nil {
-				log.Printf(
-					"failed to mark job failed: %v",
-					failErr,
-				)
-			}
+		if err := w.HandleExecutionSuccess(ctx, job); err != nil {
+			log.Print(err)
 		}
 	}
+}
+
+func (w *Worker) HandleExecutionError(ctx context.Context, job *postgres.Job, pe error) error {
+	job.RetryCount++
+
+	if job.RetryCount >= w.maxRetries {
+		job.Status = postgres.StatusFailed
+	} else {
+		job.Status = postgres.StatusPending
+	}
+
+	if failErr := w.store.Job.Fail(
+		ctx,
+		job,
+		pe,
+	); failErr != nil {
+		return fmt.Errorf(
+			"failed to persist failed state for job=%s: %w",
+			job.ID,
+			failErr,
+		)
+	}
+
+	if job.Status == postgres.StatusFailed {
+		if err := w.store.Workflow.FailWorkflow(
+			ctx,
+			job.WorkflowID,
+		); err != nil {
+			return fmt.Errorf(
+				"failed to handle job failure: %w",
+				err,
+			)
+		}
+
+		log.Printf(
+			"workflowID= %s status=%s job=%s step=%s retries=%d",
+			job.WorkflowID,
+			postgres.StatusFailed,
+			job.ID,
+			job.Step,
+			job.RetryCount,
+		)
+	}
+
+	return nil
+}
+
+func (w *Worker) HandleExecutionSuccess(ctx context.Context, job *postgres.Job) error {
+	if err := w.store.Job.Complete(ctx, job.ID); err != nil {
+		return fmt.Errorf(
+			"failed to persist completion for job=%s workflow=%s: %w",
+			job.ID,
+			job.WorkflowID,
+			err,
+		)
+	}
+
+	log.Printf(
+		"job=%s workflow=%s step=%s status=%s",
+		job.ID,
+		job.WorkflowID,
+		job.Step,
+		postgres.StatusComplete,
+	)
+
+	if job.Step != "send_confirmation" {
+		return nil
+	}
+
+	if err := w.store.Workflow.CompleteWorkflow(ctx, job.WorkflowID); err != nil {
+		return err
+	}
+
+	log.Printf(
+		"workflow=%s status=%s",
+		job.WorkflowID,
+		postgres.StatusComplete,
+	)
+
+	return nil
 }
 
 func (w *Worker) process(ctx context.Context, job *postgres.Job) error {
 	jobCtx, cancel := context.WithTimeout(
 		ctx,
-		JobTimeout*time.Second,
+		time.Duration(w.jobTimeout)*time.Second,
 	)
 	defer cancel()
 
